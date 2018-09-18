ba1c1811a2e4680d8e645435b9bf89d9232a7640
JoeyShareFood <43229487+JoeyShareFood@users.noreply.github.com>
2018-09-18 13:02:36 -0300

    Teste

---
+++ Aulas.py
@@ -0,0 +1,9 @@
+a = str(input('Qual seu nome ?'))
+if a == 'Julia':
+    print('\033[7mVai se fude\33[m')
+elif a == 'Andressa':
+    print('\033[1mVaza de perto de mim\33[m')
+elif a == 'Leticia':
+    print('Tem chocolate lá no \33[4;31mmercado\33[m')
+else:
+    print('Olá gatinha <3A')
